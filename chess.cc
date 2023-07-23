1694f854b3c2cd222004978d8f7a3a0af09acd32
k6parmar <k6parmar@uwaterloo.ca>
2023-07-23 19:38:02 -0400

    add setup skeleton

--- chess.cc
+++ chess.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "Board.h"
 #include "Player.h"
 #include "Human.h"
@@ -16,5 +18,49 @@
 using namespace std;
 
 int main() {
+    string cmd;
 
+    cin >> cmd;
+    // TODO: fill in setup skeleton structure
+    if (cmd == "setup") {
+        cout << "You have entered setup mode.\n";
+        char setup_action;
+        while (cin >> setup_action && cmd != "done") {
+            char second;
+            char third;
+            switch (setup_action) {
+                case '+':
+                    cin >> second >> third;
+                    switch (second) {
+                        case 'P':
+                            cout << "created pawn\n";
+                            break;
+                        case 'N':
+                            cout << "created knight\n";
+                            break;
+                        case 'B':
+                            cout << "created bishop\n";
+                            break;
+                        case 'R':
+                            cout << "created rook\n";
+                            break;
+                        case 'Q':
+                            cout << "created queen\n";
+                            break;
+                        case 'K':
+                            cout << "created king" << endl;
+                            break;
+                    }
+                    break;
+                case '-':
+                    cin >> second;
+                    cout << "remove " << second << '\n';
+                    break;
+                case '=':
+                    cin >> second;
+                    cout << "change colour to: " << second << '\n';
+                    break;
+            }
+        }
+    }
 }
