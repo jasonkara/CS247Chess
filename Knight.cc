c0a1546488a0de15c460105054c00246d19b0b3a
k6parmar <k6parmar@uwaterloo.ca>
2023-07-26 02:02:37 -0400

    Add King and Knight valid moves (WIP)

--- Knight.cc
+++ Knight.cc
@@ -7,5 +7,12 @@ using namespace std;
 Knight::Knight(Board* b, int x, int y, char c) : Piece{b, x, y, c, 'N'} {    }
 
 vector<pair<int,int>> Knight::getValidMoves() {
-    
+    vector<pair<int, int>> validMoveDiffs = {{-1, -2},
+                                             {-1,  2},
+                                             { 1,  2},
+                                             { 1, -2}};
+
+    for (auto p : validMoveDiffs) {
+        // checkHelper(p.first + getPosition().first, p.second + getPosition().second, validMoves);
+    }
 }
