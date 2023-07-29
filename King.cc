a59ce1830b53a59ced32c9e2cea344114614ada6
k6parmar <k6parmar@uwaterloo.ca>
2023-07-29 17:05:38 -0400

    King is functional

--- King.cc
+++ King.cc
@@ -8,12 +8,18 @@ King::King(Board* b, int x, int y, char c) : Piece{b, x, y, c, 'K'} {}
 
 vector<Coord> King::getValidMoves() {
 
-    // vector<pair<int, int>> validMoves;
-    // for (int dx = -1; dx <= 1; dx++) {
-    //     for (int dy = -1; dy <= 1; dy++) {
-    //         if (dx == 0 && dy == 0) continue;
-    //         // need to refactor checkHelper, I'm dumb.
-    //         // checkHelper(dx + getPosition().first, dy + getPosition().second, validMoves);
-    //     }
-    // }
+    vector<Coord> validMoves;
+    for (int dx = -1; dx <= 1; dx++) {
+        for (int dy = -1; dy <= 1; dy++) {
+            if (dx == 0 && dy == 0) continue;
+            Coord candidateMove = {pos.x + dx, pos.y + dy};
+            if (board->isValid(candidateMove) &&
+                (board->isEmpty(candidateMove) || isOpponent(candidateMove)))
+            {
+                validMoves.push_back(candidateMove);
+            }
+            // checkHelper(dx + getPosition().first, dy + getPosition().second, validMoves);
+        }
+    }
+    return validMoves;
 }
