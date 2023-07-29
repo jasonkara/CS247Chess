fc317a83df50eff9d0363244f12237181360dc60
k6parmar <k6parmar@uwaterloo.ca>
2023-07-29 15:56:59 -0400

    Update knight to working condition

--- Knight.cc
+++ Knight.cc
@@ -7,12 +7,21 @@ using namespace std;
 Knight::Knight(Board* b, int x, int y, char c) : Piece{b, x, y, c, 'N'} {    }
 
 vector<Coord> Knight::getValidMoves() {
-    // vector<pair<int, int>> validMoveDiffs = {{-1, -2},
-    //                                          {-1,  2},
-    //                                          { 1,  2},
-    //                                          { 1, -2}};
+    vector<Coord> validMoveDiffs = {{-1, -2},
+                                    {-1,  2},
+                                    { 1,  2},
+                                    { 1, -2}};
 
-    // for (auto p : validMoveDiffs) {
-    //     // checkHelper(p.first + getPosition().first, p.second + getPosition().second, validMoves);
-    // }
+    cerr << "in knight\n";
+
+    vector<Coord> validMoves;
+    for (auto moveDiff : validMoveDiffs) {
+        Coord candidateMove = {pos.x + moveDiff.x,  pos.y + moveDiff.y};
+        if (board->isValid(candidateMove) && 
+            (board->isEmpty(candidateMove) || isOpponent(candidateMove)))
+        {
+            validMoves.push_back(candidateMove);
+        }
+    }
+    return validMoves;
 }
