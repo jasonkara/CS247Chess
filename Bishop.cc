f5af9dee85540d39c08104920d396222fe5ddf23
k6parmar <k6parmar@uwaterloo.ca>
2023-07-29 16:54:30 -0400

    Bishop is functional

--- Bishop.cc
+++ Bishop.cc
@@ -7,5 +7,5 @@ using namespace std;
 Bishop::Bishop(Board* b, int x, int y, char c) : Piece{b, x, y, c, 'B'} { }
 
 vector<Coord> Bishop::getValidMoves() {
-    return checkHorizontal();
+    return checkDiagonal();
 }
